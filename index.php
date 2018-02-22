07063d2f5ccdf6ad9c35e778a3a9da69e17384e4
ralf.bierig <ralf.bierig@mu.ie>
2018-02-22 07:26:53 -0500

    Frontpage, feedbackprocessor stores submitted form into XML

--- index.php
+++ index.php
@@ -1,39 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Bootstrap Example</title>
+  <title>Feedback Box</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
 </head>
+
 <body>
 
-<div class="jumbotron text-center">
-  <h1>My First Bootstrap Page</h1>
-  <p>Resize this responsive page to see the effect!</p> 
-</div>
-  
-<div class="container">
-  <div class="row">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
+  <div class="jumbotron text-center">
+    <h1>Feedback Box</h1>
+    <p>Resize this responsive page to see the effect!</p>
   </div>
-</div>
+
+  <div class="container">
+
+    <!-- new stuff goes here -->
+    <form method="post" action="php/feedbackProcessor.php">
+
+      <div class="form-group">
+        <label for="module">Module: </label>
+        <select class="custom-select" id="module" name="module">
+      <option value="CS615">Internet Solutions Engineering</option>
+      <option value="CS265">Software Testing</option> </select>
+      </div>
+
+      <div class="form-group">
+        <label for="semester">Semester: </label>
+        <select class="custom-select" id="semester" name="semester">
+      <option value="Sem1_2017-18">Sem 1, 2017-18</option>
+      <option value="Sem2_2017-18">Sem 2, 2017-18</option> </select>
+      </div>
+
+      <div class="form-group">
+        <label class="radio-inline"><input type="radio" name="rating" value="1">:((</label>
+        <label class="radio-inline"><input type="radio" name="rating" value="2">:(</label>
+        <label class="radio-inline"><input type="radio" name="rating" value="3">:|</label>
+        <label class="radio-inline"><input type="radio" name="rating" value="4">:)</label>
+        <label class="radio-inline"><input type="radio" name="rating" value="5">:))</label>
+      </div>
+
+      <div class="form-group">
+        <label>Feedback:</label>
+        <textarea class="from-control" id="text" name="text" row="3" style="min-width: 100%"></textarea>
+        <div>
+          <input class="btn btn-primary" id="submit" name="submit" type="submit" value="Drop into Box" />
+    </form>
+
+    </div>
 
 </body>
+
 </html>
---
+++ php/feedbackProcessor.php
@@ -0,0 +1,35 @@
+<?php 
+
+if (isset($_POST['submit'])){
+  $module = $_POST['module'];
+  $semester = $_POST['semester'];
+  $text = $_POST['text'];
+  $rating = $_POST['rating'];
+  // print "module is $module ; semester is $semester ; text is $text ; rating is $rating";
+
+  // load the previous XML file
+  $xml = simplexml_load_file("../xml/data.xml") or die('ERROR: Cannot find the data storage');
+  
+  // add a node
+  $feedback = $xml->addChild('feedback');
+  
+  // put data in feedback node
+  $feedback->addChild('module', $module);
+  $feedback->addChild('semester', $semester);
+  $feedback->addChild('text', $text);
+  $feedback->addChild('rating', $rating);
+  
+  // save the node
+  $xml->asXML('../xml/data.xml');
+
+} else {
+  // nothing happend here, so just go back
+  header("Location: ../index.php"); 
+}
+
+
+
+
+
+
+ ?>
